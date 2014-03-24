8b30a2f83193392687737e25c02efea612bbb55c
Tim Henderson <tim.tadh@gmail.com>
2014-03-24 16:14:59 -0400

    initial commit
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+    # arguments that distutils doesn't understand
+    setuptools_kwargs = {
+        'install_requires': [
+          'ply',
+        ],
+        'provides': ['dot_tools'],
+        'zip_safe': False
+    }
+except ImportError:
+    from distutils.core import setup
+    setuptools_kwargs = {}
+
+setup(name='dot_tools',
+      version=0.1,
+      description=(
+        'Parser and Generator for the graphviz dot language.'
+      ),
+      author='Tim Henderson',
+      author_email='tadh@case.edu',
+      url='http://hackthology.com',
+      packages=['dot_tools'],
+      platforms=['unix'],
+      scripts=[],
+      **setuptools_kwargs
+)
+
